a8ae9a9dacd7da46cba60bb7623ebcdf00b61319
MathieuDeveloper <topfermathieu@yahoo.fr>
2021-10-29 22:33:15 +0200

    Update Post.cs

--- Models/Post.cs
+++ Models/Post.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -43,5 +44,11 @@ namespace TheBlogProject.Models
         [NotMapped]
         public IFormFile Image  { get; set; }
 
+        // Navigation property
+        public virtual Blog Blog { get; set; }
+        public virtual IdentityUser Author { get; set; }
+
+        public virtual ICollection<Tag> Tags { get; set; }  = new HashSet<Tag>();
+        public virtual ICollection<Comment> Comments { get; set; } = new HashSet<Comment>();
     }
 }
