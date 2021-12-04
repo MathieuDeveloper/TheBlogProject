586c4bfd9473b85e6fe3c99f81c892e4f9dfc20c
MathieuDeveloper <topfermathieu@yahoo.fr>
2021-12-04 11:25:06 +0100

    clean tag controller

--- Controllers/TagsController.cs
+++ Controllers/TagsController.cs
@@ -21,23 +21,23 @@ namespace TheBlogProject.Controllers
 
        
         // GET: Tags/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //public async Task<IActionResult> Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var tag = await _context.Tags
-                .Include(t => t.Post)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (tag == null)
-            {
-                return NotFound();
-            }
+        //    var tag = await _context.Tags
+        //        .Include(t => t.Post)
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (tag == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return View(tag);
-        }
+        //    return View(tag);
+        //}
 
         //// GET: Tags/Create
         //public IActionResult Create()
@@ -64,57 +64,57 @@ namespace TheBlogProject.Controllers
         //}
 
         // GET: Tags/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //public async Task<IActionResult> Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var tag = await _context.Tags.FindAsync(id);
-            if (tag == null)
-            {
-                return NotFound();
-            }
-            ViewData["PostId"] = new SelectList(_context.Posts, "Id", "Abstract", tag.PostId);
-            return View(tag);
-        }
+        //    var tag = await _context.Tags.FindAsync(id);
+        //    if (tag == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    ViewData["PostId"] = new SelectList(_context.Posts, "Id", "Abstract", tag.PostId);
+        //    return View(tag);
+        //}
 
-        // POST: Tags/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,PostId,AuthorId,Text")] Tag tag)
-        {
-            if (id != tag.Id)
-            {
-                return NotFound();
-            }
+        //// POST: Tags/Edit/5
+        //// To protect from overposting attacks, enable the specific properties you want to bind to.
+        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(int id, [Bind("Id,PostId,AuthorId,Text")] Tag tag)
+        //{
+        //    if (id != tag.Id)
+        //    {
+        //        return NotFound();
+        //    }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(tag);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!TagExists(tag.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["PostId"] = new SelectList(_context.Posts, "Id", "Abstract", tag.PostId);
-            return View(tag);
-        }
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            _context.Update(tag);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!TagExists(tag.Id))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    ViewData["PostId"] = new SelectList(_context.Posts, "Id", "Abstract", tag.PostId);
+        //    return View(tag);
+        //}
 
         // GET: Tags/Delete/5
         public async Task<IActionResult> Delete(int? id)
