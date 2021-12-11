4e1d5ce1d0d0e358446c75106320e15fa6eff3d8
MathieuDeveloper <topfermathieu@yahoo.fr>
2021-12-11 11:51:49 +0100

    still doing services

---
+++ Services/DataService.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using TheBlogProject.Data;
+
+namespace TheBlogProject.Services
+{
+    public class DataService
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public DataService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        public async Task ManageDataAsync()
+        {
+            //1: Seed a few Roles into the system
+            await SeedRolesAsync();
+
+            //2: Seed a few users into the system
+            await SeedUsersAsync();
+        }
+
+
+        private async Task SeedRolesAsync()
+        {
+            // if there are alreasy roles in the system, do nothing
+            if (_dbContext.Roles.Any()) { return; }
+
+            //otherwise create a few Roles
+
+
+        }
+
+       private async Task SeedUsersAsync() 
+       {
+
+       }
+
+
+    }
+}
