1d50282d2c795b7fc987a4185ce23cec6d531525
MathieuDeveloper <topfermathieu@yahoo.fr>
2021-12-19 11:52:59 +0100

    still doing

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using Microsoft.Extensions.Options;
+using MimeKit;
+using System;
 using System.Threading.Tasks;
+using TheBlogProject.ViewModels;
 
 namespace TheBlogProject.Services
 {
     public class EmailService : IBlogEmailSender
     {
+        private readonly MailSettings _mailSettings;
+
+        public EmailService(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+
         public Task SendContactEmailAsync(string emailFrom, string name, string subject, string htmlMessage)
         {
             throw new NotImplementedException();
@@ -12,7 +22,7 @@ namespace TheBlogProject.Services
 
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            throw new NotImplementedException();
+            var email = new MimeMessage();
         }
     }
 }
