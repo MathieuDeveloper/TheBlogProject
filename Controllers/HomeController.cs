5fdd5bbd1cf90660ed0652a636317c0c6952c8ea
MathieuDeveloper <topfermathieu@yahoo.fr>
2021-12-24 09:17:42 +0100

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -42,7 +42,9 @@ namespace TheBlogProject.Controllers
         public async Task<IActionResult> Contact(ContactMe model)
         {
             //This is where we will be emailing...
-            model.Message = $"{model.Message}0 <hr/> Phone: { model.Phone} ";
+            model.Message = $"{model.Message} <hr/> Phone: { model.Phone} ";
+            await _emailSender.SendContactEmailAsync(model.Email, model.Name, model.Subject, model.Message);
+            return RedirectToAction("Index");
         }
 
 
