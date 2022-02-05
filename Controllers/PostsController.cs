d5decd78a0335ef39c5c632a3de52516dc2b24e3
MathieuDeveloper <topfermathieu@yahoo.fr>
2022-02-05 09:28:15 +0100

    Posts SearcIndex
    starting full text search

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -43,17 +43,21 @@ namespace TheBlogProject.Controllers
                 p => p.ReadyStatus == ReadyStatus.ProductionReady).AsQueryable();
             if(searchTerm != null)
             {
+                searchTerm = searchTerm.ToLower();
+
                 posts = posts.Where(
-                    p => p.Title.Contains(searchTerm) ||
-                    p.Abstract.Contains(searchTerm) ||
-                    p.Content.Contains(searchTerm) ||
-                    p.Comments.Any(c => c.Body.Contains(searchTerm) ||
-                                        c.ModeratedBody.Contains(searchTerm) ||
-                                        c.BlogUser.FirstName.Contains(searchTerm) ||
-                                        c.BlogUser.LastName.Contains(searchTerm) ||
-                                        c.BlogUser.Email.Contains(searchTerm)));                    
+                    p => p.Title.ToLower().Contains(searchTerm) ||
+                    p.Abstract.ToLower().Contains(searchTerm) ||
+                    p.Content.ToLower().Contains(searchTerm) ||
+                    p.Comments.Any(c => c.Body.ToLower().Contains(searchTerm) ||
+                                        c.ModeratedBody.ToLower().Contains(searchTerm) ||
+                                        c.BlogUser.FirstName.ToLower().Contains(searchTerm) ||
+                                        c.BlogUser.LastName.ToLower().Contains(searchTerm) ||
+                                        c.BlogUser.Email.ToLower().Contains(searchTerm)));                    
             }
 
+            posts = posts.OrderByDescending(p => p.Created);
+            return View(await posts.ToPagedListAsync(pageNumber, pageSize));
 
 
         }
