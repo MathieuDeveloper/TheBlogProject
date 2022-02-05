44b95e924b0d613138343ca427dc0c937dfd0090
MathieuDeveloper <topfermathieu@yahoo.fr>
2022-02-05 10:08:14 +0100

    Update PostsController.cs

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -39,24 +39,8 @@ namespace TheBlogProject.Controllers
             var pageNumber = page ?? 1;
             var pageSize = 5;
 
-            var posts = _context.Posts.Where(
-                p => p.ReadyStatus == ReadyStatus.ProductionReady).AsQueryable();
-            if(searchTerm != null)
-            {
-                searchTerm = searchTerm.ToLower();
-
-                posts = posts.Where(
-                    p => p.Title.ToLower().Contains(searchTerm) ||
-                    p.Abstract.ToLower().Contains(searchTerm) ||
-                    p.Content.ToLower().Contains(searchTerm) ||
-                    p.Comments.Any(c => c.Body.ToLower().Contains(searchTerm) ||
-                                        c.ModeratedBody.ToLower().Contains(searchTerm) ||
-                                        c.BlogUser.FirstName.ToLower().Contains(searchTerm) ||
-                                        c.BlogUser.LastName.ToLower().Contains(searchTerm) ||
-                                        c.BlogUser.Email.ToLower().Contains(searchTerm)));                    
-            }
-
-            posts = posts.OrderByDescending(p => p.Created);
+            var posts = _blogSearchService.Search(searchTerm);
+            
             return View(await posts.ToPagedListAsync(pageNumber, pageSize));
 
 
