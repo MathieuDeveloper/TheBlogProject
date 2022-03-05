44ab965d44c342f816ddac92cdc3ad4fb6a9722d
MathieuDeveloper <topfermathieu@yahoo.fr>
2022-03-05 12:10:09 +0100

    doing ConnectionService
    starting it

---
+++ Data/ConnectionService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+
+
+namespace TheBlogProject.Data
+{
+    public class ConnectionService
+    {
+        public static string GetConnectionString(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+            return string.IsNullOrEmpty(databaseUrl) ? connectionString : BuildConnectionString(databaseUrl) ;  
+        }
+        private static string BuildConnectionString(string databaseUrl)
+        {
+
+        }
+    }
+}
